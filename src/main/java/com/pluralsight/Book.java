acb64b65c399f45bf0cdd5ab89e4293a6ba8f3fc
Sthealth <AW@example.com>
2025-10-03 14:40:10 -0400

    create book class with attributes and basic layout needed for main code

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -1,4 +1,29 @@
 package com.pluralsight;
 
 public class Book {
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String checkedOutTo;
+
+    public Book(int id, String isbn, String title, boolean isCheckedOut, String checkedOutTo) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = isCheckedOut;
+        this.checkedOutTo = checkedOutTo;
+    }
+
+    public void checkOut(String name) {
+        this.isCheckedOut = true;
+        this.checkedOutTo = name;
+        System.out.println(name + " Checked out!");
+    }
+
+    public void checkIn(String name) {
+        this.isCheckedOut = false;
+        this.checkedOutTo = "";
+        System.out.println("Thank you for checking in " + name);
+    }
 }
